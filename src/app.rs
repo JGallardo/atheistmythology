40df0004ab192d535cc7a0e546e11be09812e384
Juan Gallardo <jgallardo720@gmail.com>
2024-09-19 13:10:22 -0700

    STYLING: Added Tailwind to project

--- src/app.rs
+++ src/app.rs
@@ -20,6 +20,7 @@ pub fn App() -> impl IntoView {
             <main>
                 <Routes>
                     <Route path="" view=HomePage/>
+                    <Route path="/about" view=AboutPage/>
                     <Route path="/*any" view=NotFound/>
                 </Routes>
             </main>
@@ -35,9 +36,17 @@ fn HomePage() -> impl IntoView {
     let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
-        <h1>"Welcome to Atheist Mythology!"</h1>
-        <p>Blog under construction of myths and legends.</p>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <h1 class="text-4xl text-blue-600">"Welcome to Atheist Mythology!"</h1>
+        <p class="text-base text-gray-700">"Blog under construction of myths and legends."</p>
+        <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded hover:bg-blue-700" on:click=on_click>"Click Me: " {count}</button>
+    }
+}
+
+#[component]
+fn AboutPage() -> impl IntoView {
+    view! {
+        <h1 class="text-3xl text-blue-700">"About"</h1>
+        <p class="text-lg">"Learn more about Atheist Mythology and our mission."</p>
     }
 }
 
